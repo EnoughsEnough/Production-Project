34b4f48db645995b8d52ca83f7c4b45bcc09d446
Mark <mcherenov@mail.ru>
2024-02-20 23:51:58 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, {Suspense} from 'react';
+import './styles/index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from './pages/AboutPage/AboutPage.async'
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
+
+const App = () => {
+    const {theme, toogleTheme} = useTheme()
+
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={toogleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />}  />
+            </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
